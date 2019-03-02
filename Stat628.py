9df7a2bd7da65395a948b38ff8121deac3aedf82
Sylvia Wong <sylviawong3527@gmail.com>
2019-03-02 14:26:29 -0600

    Add files via upload

--- Stat628.py
+++ Stat628.py
@@ -1,8 +1,9 @@
-#Import Essential Packages
+# Import Essential Packages
 import json
 import sklearn
 import collections
 import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import nltk
@@ -14,24 +15,29 @@ import textblob
 import nltk.sentiment
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
+from pyitlib import discrete_random_variable as drv
+import time
 
-#一些想做的事：
-#对于每个评价，我们可以用business找到对应的商家，对于不同的商业类型给与不同的答案/评价
+# 一些想做的事：
+# 对于每个评价，我们可以用business找到对应的商家，对于不同的商业类型给与不同的答案/评价
 # Deal with mis-spelling
 
 
 ###### STOPWORDS ######
-stopwords = set(nltk.corpus.stopwords.words('english')) #We choose some stopwords from library
-keep1 = {"no","nor","not","don","don't", "shouldn't","ain","aren","aren't",'couldn',"couldn't",'didn','doesn',
-        "doesn't","didn't","wouldn't",'wouldn',"won't","won","shan't","hasn't","hadn't",'hadn','hasn','haven',"haven't",
-        "weren't",'weren',"wasn't",'wasn',"very","couldn't",'isn', 'mightn',"mightn't",'mustn',"mustn't", 'needn',"needn't",
-        "isn't",'shan',"shan't", 'shouldn',"shouldn't",} #Negative words
-keep2 = {"all","above","below","up","down"} #Something useful in stopwords
+stopwords = set(nltk.corpus.stopwords.words('english'))  # We choose some stopwords from library
+keep1 = {"no", "nor", "not", "don", "don't", "shouldn't", "ain", "aren", "aren't", 'couldn', "couldn't", 'didn',
+         'doesn',
+         "doesn't", "didn't", "wouldn't", 'wouldn', "won't", "won", "shan't", "hasn't", "hadn't", 'hadn', 'hasn',
+         'haven', "haven't",
+         "weren't", 'weren', "wasn't", 'wasn', "very", "couldn't", 'isn', 'mightn', "mightn't", 'mustn', "mustn't",
+         'needn', "needn't",
+         "isn't", 'shan', "shan't", 'shouldn', "shouldn't", }  # Negative words
+keep2 = {"all", "above", "below", "up", "down"}  # Something useful in stopwords
 for word in keep1:
     stopwords.remove(word)
 for word in keep2:
     stopwords.remove(word)
-add1 = set("'ve") # Add some stopwords
+add1 = set("'ve")  # Add some stopwords
 for word in add1:
     stopwords.add(word)
 
@@ -39,17 +45,22 @@ for word in add1:
 porter_stemmer = PorterStemmer()
 wordnet_lemmatizer = WordNetLemmatizer()
 
-
 ######## GOOD WORD AND BAD WORD ########
 # Read in Pos & Neg dictionary by Minqing Hu and Bing Liu
 # https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html#lexicon
-good_word = set(pd.read_csv('/Users/moran/Google_Drive/Course/628/Proj2/opinion-lexicon-English/positive-words.txt', header = None, names = ['word'])['word'])
-bad_word = set(pd.read_csv('/Users/moran/Google_Drive/Course/628/Proj2/opinion-lexicon-English/negative-words.txt', encoding = "ISO-8859-1", header = None, names = ['word'])['word'])
-
+good_word = set(
+    pd.read_csv('/Users/moran/Google_Drive/Course/628/Proj2/opinion-lexicon-English/positive-words.txt', header=None,
+                names=['word'])['word'])
+bad_word = set(pd.read_csv('/Users/moran/Google_Drive/Course/628/Proj2/opinion-lexicon-English/negative-words.txt',
+                           encoding="ISO-8859-1", header=None, names=['word'])['word'])
+# Add NEG dictionary
+good_tmp = set(x + '_NEG' for x in bad_word)
+bad_tmp = set(x + '_NEG' for x in good_word)
+good_word = good_word | good_tmp
+bad_word = bad_word | bad_tmp
 
 ###### TOKENIZING ######
 def tokenization_and_stemming_all(text):
-
     ### FEATURE: attitude ###
     # Measure the intensity of reviewer's emotion
     # There are two types of emotion I choose
@@ -58,9 +69,8 @@ def tokenization_and_stemming_all(text):
     # 2. The occurance of several continous ? or ! also indicates a strong attitude
     attitude = 0  # Initialize reviewer's attitude
     attitude += len(re.findall(r'[A-Z]{4,}', text))
-    attitude += len(re.findall(r'[?!]+', text))
-
-
+    attitude += len(re.findall(r'[?!]+', text)) # !? exists
+    attitude += len(re.findall(r'[?!]{2,}', text)) # !? occurance >= 2
 
     ### TEXT CLEANING ###
     # nltk.word_tokenize would split [don't] to [do, n't]
@@ -70,7 +80,7 @@ def tokenization_and_stemming_all(text):
     text = re.sub("[nN]'", " not", text)
 
     # Change any punctuation repeat more than once to period.
-    text = re.sub(r'[^A-Za-z0-9_\ ]{2,}', " . ", text)
+    text = re.sub(r'[^A-Za-z0-9_\ ]{2,}', " . ", text) # reduce ! ?
 
     # mark_negation function does not recognize comma as seperator but so does period
     text = re.sub(',', " . ", text)
@@ -79,7 +89,8 @@ def tokenization_and_stemming_all(text):
     text = re.sub(r'[^A-Za-z_\ .!?;:]', " ", text)
 
     # Tokenizing starts
-    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent) if word.lower() not in stopwords]
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent) if
+              word.lower() not in stopwords]
     # for sentence in nltk.sent_tokenize(text):
     #     for word in nltk.word_tokenize(sentence):
     #     # for word in sentence.split():
@@ -107,11 +118,9 @@ def tokenization_and_stemming_all(text):
     return (attitude, good_num, bad_num, stems)
 
 
-
-def token_wrapper(text): # Wrap up the tokenizer
+def token_wrapper(text):  # Wrap up the tokenizer
     attitude, good_num, bad_num, stems = tokenization_and_stemming_all(text)
     return stems
-
 
 
 ###### TEST DATA ######
@@ -140,7 +149,18 @@ def token_wrapper(text): # Wrap up the tokenizer
 ###### 测试集结束 ######
 
 
+###### Lexicon-Based Sentiment Analysis ######
+
+# 中文称作词典法
+# Predicition RMSE: 1.0+
+# Can not detect sarcasm or other form of emotion transitions.
+# It is a real tough question, let's just first leave it here.
+
+# sklearn.hashingvetorize 培根的方法
+
+
 ###### FEATURE SELECTION ######
+
 # Read in Data
 ourfile = '/Users/moran/Google_Drive/Course/628/Proj2/review_1k.json'
 out = []
@@ -150,13 +170,16 @@ with open(ourfile, 'r') as fh:
         out.append(d)
 
 df = pd.DataFrame(out)
-vect = sklearn.feature_extraction.text.CountVectorizer(tokenizer = token_wrapper)
+
+vect = sklearn.feature_extraction.text.CountVectorizer(tokenizer=token_wrapper)
 dat_x, dat_y = vect.fit_transform(df['text']), df['stars']
+
 
 ### 1. INFORMATION GAIN ###
 
 # Codes are from Stackoverflow
 # https://stackoverflow.com/questions/25462407/fast-information-gain-computation
+# Time: very fast
 def information_gain(X, y):
     def _calIg():
         entropy_x_set = 0
@@ -171,7 +194,7 @@ def information_gain(X, y):
                 probs = classTotCnt[c] / float(tot - featureTot)
                 entropy_x_not_set = entropy_x_not_set - probs * np.log(probs)
         return entropy_before - ((featureTot / float(tot)) * entropy_x_set
-                             +  ((tot - featureTot) / float(tot)) * entropy_x_not_set)
+                                 + ((tot - featureTot) / float(tot)) * entropy_x_not_set)
 
     tot = X.shape[0]
     classTotCnt = {}
@@ -192,7 +215,7 @@ def information_gain(X, y):
     information_gain = []
     for i in range(0, len(nz[0])):
         if (i != 0 and nz[0][i] != pre):
-            for notappear in range(pre+1, nz[0][i]):
+            for notappear in range(pre + 1, nz[0][i]):
                 information_gain.append(0)
             ig = _calIg()
             information_gain.append(ig)
@@ -210,35 +233,81 @@ def information_gain(X, y):
 
     return np.asarray(information_gain)
 
-ans = information_gain(dat_x, dat_y)
-name = vect.get_feature_names()
-combine = [(name[i], ans[i]) for i in range(len(ans))]
-combine.sort(key = lambda x: x[1], reverse = True)
-combine[:20]
+
+name = vect.get_feature_names() # Get the word
+ans_IG = information_gain(dat_x, dat_y)
+combine_IG = [(name[i], ans_IG[i]) for i in range(len(ans_IG))]
+combine_IG.sort(key=lambda x: x[1], reverse=True)
+
+dat_plot = pd.DataFrame(combine_IG)
+dat_plot.columns = ['word', 'IG']
+threshold_IG = np.mean(dat_plot['IG']) + 1.645 * np.var(dat_plot['IG']) ** 0.5
+dat_plot = dat_plot.iloc[:sum(1 for x in dat_plot['IG'] if x > threshold_IG), :]
+dat_plot.plot()
+plt.show()
+feature_IG = set(dat_plot['word'])
+len(feature_IG)
+
+
+### 2. POINTWISE MUTUAL INFORMATION (PMI) ###
+# Use package pyitlib
+# Time: One word takes about 0.0235s (1000 rows)
+def PMI(X,y):
+    def one_class_PMI(X, y, num):
+        y_tmp = (y == num).astype(float)
+        X_tmp = (X > 0).astype(float).toarray()
+
+        def MI_with_y(X):
+            return drv.information_mutual(X, y_tmp)
+
+        return np.apply_along_axis(MI_with_y, 0, X_tmp)
+
+    def PMI_classes(num):
+        return one_class_PMI(X, y, num)
+
+    l = list(map(PMI_classes, [1, 2, 3, 4, 5]))
+
+    return np.concatenate(l,axis = 0).reshape(5,-1).mean(axis = 0)
+
+ans_PMI = PMI(dat_x,dat_y)
+combine_PMI = [(name[i], ans_PMI[i]) for i in range(len(ans_PMI))]
+combine_PMI.sort(key=lambda x: x[1], reverse=True)
+
+dat_plot = pd.DataFrame(combine_PMI)
+dat_plot.columns = ['word', 'PMI']
+threshold_PMI = np.mean(dat_plot['PMI']) + 1.645 * np.var(dat_plot['PMI']) ** 0.5
+dat_plot = dat_plot.iloc[:sum(1 for x in dat_plot['PMI'] if x > threshold_PMI), :]
+dat_plot.plot()
+plt.show()
+feature_PMI = set(dat_plot['word'])
+len(feature_PMI)
+
+feature_FINAL = feature_PMI.intersection(feature_IG) # 279, Too many feature
+
+
+### 3. CHI-SQUARED ###
+
+from scipy.stats import chi2_contingency
+X_tt = dat_x[:,0].toarray()
+chi2_contingency(np.array(X_tt,y))
 
 
 
 
+# Naive Bayes
+np.apply_along_axis(my_func, 0, b)
 
-#Naive Bayes
-
-
-
-
-
-
-
-
-#Train-Test split
+# Train-Test split
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(df['text'].values,
-                 df['stars'].values,
-                 test_size=0.2)
+                                                                            df['stars'].values,
+                                                                            test_size=0.2)
 
-vect = sklearn.feature_extraction.text.CountVectorizer(tokenizer = token_wrapper)
+vect = sklearn.feature_extraction.text.CountVectorizer(tokenizer=token_wrapper)
 tf_train = vect.fit_transform(X_train)
 tf_test = vect.transform(X_test)
 
 from sklearn.naive_bayes import MultinomialNB
+
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
 clf.score(X_train, y_train)
@@ -249,23 +318,19 @@ sum(x ** 2 for x in res)
 res = [3.7 - y_test[i] for i in range(len(y_test))]
 sum(x ** 2 for x in res)
 
-
-#define vectorizer parameters
+# define vectorizer parameters
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_model = TfidfVectorizer(max_df= 0.99 , max_features=2000,
-                                 min_df=0.1, stop_words='english',
-                                 use_idf=True, tokenizer=tokenization_and_stemming, ngram_range=(1,1))
 
-tfidf_matrix = tfidf_model.fit_transform(df['text'].values) #fit the vectorizer to synopses
+tfidf_model = TfidfVectorizer(max_df=0.99, max_features=2000,
+                              min_df=0.1, stop_words='english',
+                              use_idf=True, tokenizer=tokenization_and_stemming, ngram_range=(1, 1))
 
-#Train-Test split
+tfidf_matrix = tfidf_model.fit_transform(df['text'].values)  # fit the vectorizer to synopses
+
+# Train-Test split
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(tfidf_matrix,
-                 df['stars'].values,
-                 test_size=0.2)
+                                                                            df['stars'].values,
+                                                                            test_size=0.2)
 
-
-print ("In total, there are " + str(tfidf_matrix.shape[0]) + \
+print("In total, there are " + str(tfidf_matrix.shape[0]) + \
       " synoposes and " + str(tfidf_matrix.shape[1]) + " terms.")
-
-
-
